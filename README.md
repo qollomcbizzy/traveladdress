8f65bc0207ad49cb7c8f7181e4335e6bc8f6375d
collo <qollomcbizzy64@gmail.com>
2017-05-08 19:16:19 +0300

    added the copy  constructor

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ this project show the places that i  have gone / visited
 2. Create a form first from the html part and css to display none to the location details
 3. Create the user interface for the input details
 4. Create a constructor in the user interface
+5. Create a click function when the user clicks on the location it displays details
--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
 //back-end for my address book
 //first function of the propeties of object place
-function Place(name,country,county,town,timeOfYear,notes){
-  this.loacationName=name;
-  this.locationCountry=country;
-  this.locationCounty=county;
+function Place(name,town,county,country,timeOfYear,notes){
+  this.locationName=name;
   this.locationCity=town;
+  this.locationCounty=county;
+  this.locationCountry=country;
   this.locationTime=timeOfYear;
   this.shortnote=notes;
-}
+};
 //user interface logic
 $(document).ready(function(){
-$("form#destination").submit(function(){
-  var inputedName=$("input#new-name").val();
-  var inputedTown=$("input#new-town").val();
-  var inputedCounty=$("input#new-county").val();
-  var inputedCountry=$("input#new-country").val();
-  var inputedTime=$("input#new-date").val();
-  var inputedNotes=$("input#new-name").val();
-  var newLocation=new Place(inputedName,inputedTown,inputedCounty,inputedCountry,inputedTime,inputedNotes);
+$("form#destination").submit(function(event){
+  event.preventDefault();
+  var inputtedName=$("input#new-name").val();
+  var inputtedTown=$("input#new-town").val();
+  var inputtedCounty=$("input#new-county").val();
+  var inputtedCountry=$("input#new-country").val();
+  var inputtedTime=$("input#new-date").val();
+  var inputtedNotes=$("input#new-notes").val();
+  var newLocation=new Place(inputtedName,inputtedTown,inputtedCounty,inputtedCountry,inputtedTime,inputtedNotes);
+  $("ul#place").append("<li><span class='location'>"+ newLocation.inputtedName +"</span></li>");
+  $(".location").click(function(){
+    $("#show-location").show();
+    $("#show-location h2").text(newLocation.locationName);
+    $(".town").text(newLocation.locationCity);
+    $(".county").text(newLocation.locationCounty);
+    $(".country").text(newLocation.locationCountry);
+    $(".year").text(newLocation.locationTime);
+    $(".note").text(newLocation.shortnote);
+  });
+  $("input#new-name").val();
+  $("input#new-town").val();
+  $("input#new-county").val();
+  $("input#new-country").val();
+  $("input#new-date").val()
+  $("input#new-notes").val()
 });
 });
