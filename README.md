a041f415c65be4ebcb2e0886306ed3c0a80dab9f
collo <qollomcbizzy64@gmail.com>
2017-05-08 15:19:56 +0300

    created the first specification with declaring properties to the object place

---
+++ README.md
@@ -0,0 +1,10 @@
+# ADDRESS BOOK FOR MY TRAVELS
+---
+### AUTHOR
+---
+KIPROTICH COLLINS
+#### PROJECT DESCRIPTION
+---
+this project show the places that i  have gone / visited
+#### PROJECT DESCRIPTION
+1.Create a back-end interface with the properties of a place
---
+++ js/script.js
@@ -0,0 +1,9 @@
+//back-end for my address book
+//first function of the propeties of object place
+function PLACE(country,county,town,timeOfYear,notes){
+  this.country=country;
+  this.county=county;
+  this.city=town;
+  this.time=timeOfYear;
+  this.shortnote=notes;
+}
