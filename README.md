3c240d599dab14a2bbae4e6e5b90ea5b817c6416
collo <qollomcbizzy64@gmail.com>
2017-05-08 16:01:09 +0300

    created the user interface for the user interface

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ KIPROTICH COLLINS
 ---
 this project show the places that i  have gone / visited
 #### PROJECT DESCRIPTION
-1.Create a back-end interface with the properties of a place
-2.Create a form first from the html part
+1. Create a back-end interface with the properties of a place
+2. Create a form first from the html part and css to dislay none to the location details
+3. Create the user interface for the iput details
--- js/script.js
+++ js/script.js
@@ -10,5 +10,12 @@ function PLACE(name,country,county,town,timeOfYear,notes){
 }
 //user interface logic
 $(document).ready(function(){
-
+$("form#destination").submit(function(){
+  var inputedName=$("input#new-name").val();
+  var inputedTown=$("input#new-town").val();
+  var inputedCounty=$("input#new-county").val();
+  var inputedCountry=$("input#new-country").val();
+  var inputedTime=$("input#new-date").val();
+  var inputedNotes=$("input#new-name").val();
+});
 });
